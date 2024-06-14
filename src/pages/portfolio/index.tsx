9cd11250a2ad421b9347b930538bc328ffc6ed5a
babyjazz <bbos1@windowslive.com>
2024-06-15 02:18:06 +0700

    move get portfolio to middleware

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,29 +1,8 @@
-import { useCallback, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { WssFunctionNameMessageType } from '@/enums/websocket'
+import { useSelector } from 'react-redux'
 import { portfolioSelectors } from '@/store/portfolio'
-import { websocketSlice } from '@/store/websocket'
 
 export default function Portfolio() {
-  const dispatch = useDispatch()
-  const { isConnected } = useSelector(websocketSlice.selectSlice)
-  // const portfolio = useSelector(portfolioSelectors.portfolio)
-
-  const getLevel1Sumamry = useCallback(() => {
-    dispatch(
-      websocketSlice.actions.send({
-        n: WssFunctionNameMessageType.GetLevel1Summary,
-        o: JSON.stringify({ OMSId: 1 }),
-      }),
-    )
-  }, [dispatch])
-
-  useEffect(() => {
-    console.log('debug #11')
-    if (isConnected) {
-      getLevel1Sumamry()
-    }
-  }, [getLevel1Sumamry, isConnected])
+  const portfolio = useSelector(portfolioSelectors.portfolio)
 
   return (
     <div>
