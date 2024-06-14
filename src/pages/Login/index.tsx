b0eb65f97588a8c2089f91e991de0a95312cbcdf
babyjazz <bbos1@windowslive.com>
2024-06-15 00:09:30 +0700

    remove unused code

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import Button from '@/components/ui/Button'
 import Input from '@/components/ui/Input'
 import { routePathnames } from '@/constants/routesPathName'
@@ -48,6 +48,7 @@ export default function Login() {
     <div className="m-auto flex h-screen w-[537px] items-center">
       <div className="w-full rounded border-[1px] border-theme-default bg-transparent p-6">
         <h1>Log Into Bitazza Global</h1>
+
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
           <div className="mt-10 flex flex-col">
             <label>Email</label>
