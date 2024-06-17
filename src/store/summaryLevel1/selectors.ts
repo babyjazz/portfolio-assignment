1c06e1faafd42398ebca1e2bbe5183a1b9273717
babyjazz <bbos1@windowslive.com>
2024-06-18 01:44:03 +0700

    add firebase deployment

--- src/store/summaryLevel1/selectors.ts
+++ src/store/summaryLevel1/selectors.ts
@@ -1,5 +1,5 @@
 import { RootState } from '@/store'
 import { ISummaryLevel1 } from '@/types/summaryLevel1'
 
-export const summaryLevel1 = (state: RootState): ISummaryLevel1 =>
+export const summaryLevel1 = (state: RootState): ISummaryLevel1 | null =>
   state.summaryLevel1
